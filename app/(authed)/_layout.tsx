aa26715b47e31b0d2eea9c4e565c85797194a492
Julius <juliuscecilia33@gmail.com>
2025-03-28 21:54:03 -0500

    removed auth

--- app/(authed)/_layout.tsx
+++ app/(authed)/_layout.tsx
@@ -1,14 +1,5 @@
-import { useAuth } from "@/context/AuthContext";
-import { Redirect, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function AppLayout() {
-  const { user } = useAuth(); // Access authenticated user
-
-  console.log("current user in authed layout:", user);
-
-  if (!user) {
-    return <Redirect href="/onboard/auth/login" />;
-  }
-
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,13 @@
 // import { AuthenticationProvider, useAuth } from "@/context/AuthContext";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { AWS_APP_CLIENT_ID, AWS_REGION, AWS_USER_POOL_ID } from "@/env-vars";
-import { Amplify, Auth } from "aws-amplify";
-import amplifyconfig from "../src/amplifyconfiguration.json";
-import { AuthProvider } from "@/context/AuthContext"; // Import AuthProvider
 import React from "react";
 
 export default function RootLayout() {
-  Amplify.configure(amplifyconfig);
-
   return (
     <>
-      <AuthProvider>
-        <StatusBar style="dark" />
-        <Slot />
-      </AuthProvider>
+      <StatusBar style="dark" />
+      <Slot />
     </>
   );
 }
